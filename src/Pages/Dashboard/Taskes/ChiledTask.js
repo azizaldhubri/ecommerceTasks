9a7ec187d822faa0a83c151bb423b78766d8b31d
azizaldhubri <azizaldhubri@gmail.com>
2024-09-17 00:27:52 +0300

    Update ChiledTask.js

--- src/Pages/Dashboard/Taskes/ChiledTask.js
+++ src/Pages/Dashboard/Taskes/ChiledTask.js
@@ -300,8 +300,7 @@ const handleOptionChange =async (e) => {
                   <div className=" d-flex gap-2 align-items-center justify-content-center  flex-column" >
                     <img src={require('../../../Assets/files/rar.jpg')} width='80px' height='60px' alt="docs"></img>
                     <p className="m-0"style={{fontSize:'12px'}}>{File_Name(file,index)}</p>
-                       \
-                     <a className="fs-6"  href={`https://free-001.yemenhosting.com/api/download/${File_Path(file,index)}`} >Download</a>
+                   <a className="fs-6"  href={`https://free-001.yemenhosting.com/api/download/${File_Path(file,index)}`} >Download</a>
                   </div>
                   
                 :(file.split('.').pop())}
