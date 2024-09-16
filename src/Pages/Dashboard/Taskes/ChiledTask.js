21bc4a911c19a27a49573542d5bbfd82d1220dfe
azizaldhubri <azizaldhubri@gmail.com>
2024-09-16 04:08:52 +0300

    Update ChiledTask.js

--- src/Pages/Dashboard/Taskes/ChiledTask.js
+++ src/Pages/Dashboard/Taskes/ChiledTask.js
@@ -271,7 +271,7 @@ const handleOptionChange =async (e) => {
                       <img key={index} src={`https://free-001.yemenhosting.com/api/backend/storage/app/public/assets/${File}`} width='60px'height='60px' alt="img"></img>                   
                       <p className="m-0">{File_Name(file,index)}</p>
                       {/* <a  href={`https://free-001.yemenhosting.com/api/storage/${file}`} >Download</a>  */}
-                      <a className="fs-6"  href={`https://free-001.yemenhosting.com/backend/storage/app/public/assets/download/${File_Path(file,index)}`} >Download</a>
+                      <a className="fs-6"  href={`https://free-001.yemenhosting.com/api/backend/storage/app/public/assets/download/${File_Path(file,index)}`} >Download</a>
                   </div>
                   
                   :(file.split('.').pop())==='docx'?(
